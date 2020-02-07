0aa43ac1d60658382b6cfe22c460cd94a2b1f6b7
Fede <dfedericovera@gmail.com>
2020-02-07 15:17:00 -0300

    Update

---
+++ Navegador/Navegador/Archivos/Texto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        public string archivo;
+        public Texto(string archivo)
+        {
+            this.archivo =AppDomain.CurrentDomain.BaseDirectory + archivo;
+        }
+
+        public bool Guardar(string datos)
+        {
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(archivo, true))
+                {
+                    file.WriteLine(datos);
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        //public bool leer(out List<string> datos)
+        public bool Leer(out List<string> datos)
+        {
+            datos = new List<string>();
+            System.IO.StreamReader file = new System.IO.StreamReader(archivo);
+            try
+            {
+                do
+                {
+                    datos.Add(file.ReadLine());
+                }while (!file.EndOfStream);
+                file.Close();
+                return true;
+            }
+            catch (Exception)
+            {                
+                return false;
+            }
+        }
+    }
+}
---
+++ Navegador/Navegador/Hilo/Descargador.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        public string html;
+        private Uri direccion;
+
+        public delegate void ProgresoDescargaCallback(int progreso);
+        public event ProgresoDescargaCallback Progresodescarga;
+        public delegate void FinDescargaCallback(string html);
+        public event FinDescargaCallback FinDescarga;
+        public Descargador(string direccion)
+        {
+            if (direccion.ToLower().StartsWith("http://"))
+            {
+                this.html = direccion;
+            }
+            else
+            {
+                this.html = "http://" + direccion;
+            }
+            this.direccion = new Uri(html);
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += WebClientDownloadProgressChanged ;
+                cliente.DownloadStringCompleted += WebClientDownloadCompleted ;
+
+                cliente.DownloadStringAsync(this.direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.Progresodescarga(e.ProgressPercentage);
+        }
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            try
+            {
+                this.FinDescarga(e.Result);
+            }
+            catch(Exception exc)
+            {
+                this.FinDescarga(exc.InnerException.Message);
+            }
+            //e.Result
+        }
+    }
+}
